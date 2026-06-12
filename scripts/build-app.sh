e991cdfca1eaf6e5532c681d1e5de0e7c61cba93
gabrielMalonso <gabriel_alonso_@outlook.com>
2026-06-12 13:47:19 -0300

    Package updated app build
    - Bump bundled app build number to 3
    - Refresh release artifacts from the packaged build

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -38,7 +38,7 @@ cat > "$CONTENTS_DIR/Info.plist" <<'PLIST'
     <key>CFBundleShortVersionString</key>
     <string>0.1.1</string>
     <key>CFBundleVersion</key>
-    <string>2</string>
+    <string>3</string>
     <key>CFBundleURLTypes</key>
     <array>
         <dict>
